6c3d3fd80a0b075122f6f09d363e647b839a5326
mcl-playz <99476285+mcl-playz@users.noreply.github.com>
2022-09-04 19:44:25 +0800

    Add files via upload

---
+++ Install BlinkDetector Libs.bat
@@ -0,0 +1,19 @@
+@echo off
+
+python.exe -m pip install --upgrade pip
+timeout 1
+cls
+pip install opencv-python
+timeout 1
+cls
+pip install cvzone
+timeout 1
+cls
+pip install mediapipe
+timeout 1
+cls
+pip install pynput
+
+cls
+echo Required libraries installed!
+pause
